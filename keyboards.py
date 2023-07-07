821b7dd220ec447dc1c1736a6de94f56b119cac0
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2023-07-08 00:29:13 +0300

    add items from db

--- keyboards.py
+++ keyboards.py
@@ -10,8 +10,20 @@ menuitem5 = InlineKeyboardButton(text="⚙️BOT SETTINGS", callback_data='setti
 markup.add(menuitem1, menuitem2, menuitem3, menuitem4, menuitem5,)
 
 markup2 = InlineKeyboardMarkup(row_width=2)
-menuitem1 = InlineKeyboardButton(text="Spa Menu", url='https://grandbazar.io/ru/collection/robocraft')
+menuitem1 = InlineKeyboardButton(text="Spa Menu", callback_data='spa menu')
 menuitem2 = InlineKeyboardButton(text='📞WhatsApp', url="https://wa.me")
 menuitem3 = InlineKeyboardButton(text='↩️Main menu', callback_data='main menu')
 markup2.add(menuitem1, menuitem2, menuitem3)
+
+markup3 = InlineKeyboardMarkup(row_width=1)
+menuitem1 = InlineKeyboardButton(text="Massage Menu(2)", callback_data='massage menu')
+menuitem2 = InlineKeyboardButton(text='Scrub(1)', callback_data='scrub')
+menuitem3 = InlineKeyboardButton(text='↩️Back', callback_data='back_spa')
+markup3.add(menuitem1, menuitem2, menuitem3)
+
+markup4 = InlineKeyboardMarkup(row_width=2)
+menuitem1 = InlineKeyboardButton(text='↩️Back', callback_data='️back_spa_menu')
+markup4.add(menuitem1)
+
+
 
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from aiogram import Bot, Dispatcher, types, executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import config
-from keyboards import markup, markup2
+from keyboards import markup, markup2, markup3, markup4
 import database as db
+
 
 bot = Bot(config.bot_token)
 dp = Dispatcher(bot=bot)
@@ -13,12 +14,15 @@ async def on_startup(_):
     await db.db_start()
     print("started")
 
+
 @dp.message_handler(commands=['start'])
 async def command_start(msg: types.Message):
     await db.cmd_start_db(msg.from_user.id)
     await bot.send_photo(chat_id=msg.chat.id,
-                         photo='https://gidnabali.ru/user_files/tunjungsarispa3.jpg',
-                         caption='Welcome to the Official TGBot TSPA!',
+                         photo='https://avatars.mds.yandex.net/get-altay/1868686/2a0000016a0afc45ddadefd260492d2a734a/orig',
+                         caption='Welcome to the Official TGBot TSPA! Experience the ultimate luxury spa experience at Jaens'
+                                 ' Spa Ubud, with a unique combination of a traditional Balinese spa combined with modern'
+                                 ' facilities and treatments.',
                          reply_markup=markup)
 
 
@@ -34,10 +38,39 @@ async def callback_query_keyboad(callback_query: types.CallbackQuery):
     elif callback_query.data == "main menu":
         await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
         await bot.send_photo(chat_id=callback_query.from_user.id,
-                             photo='https://gidnabali.ru/user_files/tunjungsarispa3.jpg',
-                             caption='Welcome to the Official TGBot TSPA!',
+                             photo='https://avatars.mds.yandex.net/get-altay/1868686/2a0000016a0afc45ddadefd260492d2a734a/orig',
+                             caption='Welcome to the Official TGBot TSPA! '
+                                     'Experience the ultimate luxury spa experience at Jaens Spa Ubud, with a unique combination of '
+                                     'a traditional Balinese spa combined with modern facilities and treatments.',
                              reply_markup=markup)
-
+    elif callback_query.data == 'spa menu':
+        await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+        await bot.send_photo(chat_id=callback_query.from_user.id,
+                             photo='https://www.flokq.com/blog/wp-content/uploads/2021/01/5-bali-spa-800x600.jpg',
+                             caption='Ꮥelect category💫',
+                             reply_markup=markup3)
+    elif callback_query.data == 'back_spa':
+        await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+        await bot.send_photo(chat_id=callback_query.from_user.id,
+                             photo='https://www.realbali.com/wp-content/uploads/2013/11/bali-massage-for-couple.jpg',
+                             caption='Soak up panoramic views across the Petanu River gorge at the beautiful Lembah Spa. Boasting one of the most tranquil settings and renowned for being one of the best spas in Bali, Lembah spa is the perfect location to take care of your mind, body and soul. Lembah Spa’s menu contains a specialty mix of western and traditional Balinese wellness treatments – guaranteed to place your body in a pure state of relaxation.!',
+                             reply_markup=markup2)
+    elif callback_query.data == 'massage menu':
+        await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+        await db.sql_read_one(callback_query)
+        await bot.send_message(chat_id=callback_query.from_user.id, text="You can go back to categories!💎",
+                               reply_markup=markup4)
+    elif callback_query.data == 'scrub':
+        await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+        await db.sql_read_two(callback_query)
+        await bot.send_message(chat_id=callback_query.from_user.id, text="You can go back to categories!💎",
+                               reply_markup=markup4)
+    elif callback_query.data == '️back_spa_menu':
+        await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+        await bot.send_photo(chat_id=callback_query.from_user.id,
+                             photo='https://www.flokq.com/blog/wp-content/uploads/2021/01/5-bali-spa-800x600.jpg',
+                             caption='Ꮥelect category💫',
+                             reply_markup=markup3)
 
 
 if __name__ == "__main__":
