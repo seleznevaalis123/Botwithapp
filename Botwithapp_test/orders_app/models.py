40f8017114ef8c1e1a9a1556809832711d969d46
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2023-07-29 12:31:46 +0300

    add time zone/change data types Django

--- Botwithapp_test/orders_app/models.py
+++ Botwithapp_test/orders_app/models.py
@@ -25,28 +25,36 @@ class Spa_items(models.Model):
         verbose_name = 'spa item'
         verbose_name_plural = 'spa items'
 
-    item_name = models.IntegerField(verbose_name='Spa item name')
+    item_name = models.TextField(verbose_name='Spa item name')
     item_desc = models.TextField(verbose_name='Spa item desc')
     item_price = models.IntegerField(verbose_name='Spa item price')
-    item_photo = models.BinaryField(verbose_name='Spa item photo')
+    item_photo = models.ImageField(upload_to='photo/', verbose_name='Spa item photo')
 
     def __str__(self):
-        return f'{self.item_name} {self.item_desc}'
+        return f'{self.item_name}'
 
 
 class Orders(models.Model):
     '''Orders'''
+
+    Order_states = [
+        ('NEW', 'new'),
+        ('IN_PROGRESS', 'in_progress'),
+        ('DONE', 'done'), ]
     class Meta:
         db_table = 'user_orders'
         verbose_name = 'order'
         verbose_name_plural = 'order'
 
-    user_id = models.ForeignKey(Users, on_delete=models.RESTRICT, verbose_name='item_name')
+    user_id = models.ForeignKey(Users, on_delete=models.RESTRICT, verbose_name='User')
     spa_item = models.ForeignKey(Spa_items, verbose_name='Spa item', on_delete=models.RESTRICT)
     created_dt = models.DateTimeField(verbose_name='Created', auto_now_add=True)
-    last_updated_dt = models.TextField(verbose_name='Last update', blank=True, null=True)
-    status = models.TextField(verbose_name='State')
+    last_updated_dt = models.DateTimeField(verbose_name='Last update', blank=True, null=True)
+    status = models.CharField(verbose_name='State', max_length=20, choices=Order_states)
     start_time = models.TimeField(verbose_name='Start time')
+
+    def __str__(self):
+        return self.status
 
 
 
